e68b07f3e112066a938d23a6798b8550d021e118
Dominika Janus <demi.janus@gmail.com>
2022-06-12 11:32:39 +0200

    enhanced training set

--- imprre/run_roberta_eval.sh
+++ imprre/run_roberta_eval.sh
@@ -12,7 +12,7 @@ for SEED in 23;
 #do python3 train_retacred.py --model_name_or_path clarin-pl/roberta-polish-kgr10 --input_format typed_entity_marker_punct --seed $SEED --run_name roberta --num_class 9;
 
 #polskie z zanuerzanimi herberta
-do python standalone_evaluation.py --model_name_or_path allegro/herbert-large-cased --input_format typed_entity_marker_punct --seed 23 --run_name herbertInq --num_class 9;
+do python standalone_evaluation.py --model_name_or_path allegro/herbert-large-cased --input_format typed_entity_marker_punct --seed 23 --run_name herbertInq --num_class 2;
 
 
 #angielskie zanurzenia
